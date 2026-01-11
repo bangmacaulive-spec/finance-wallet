7ff8dea4fdd61726f3317df8b535553155647343
bangmacaulive-spec <bangmacaulive@gmail.com>
2026-01-11 07:30:05 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+const CACHE = "finance-wallet-v1";
+const ASSETS = [
+  "/finance-wallet/",
+  "/finance-wallet/index.html",
+  "/finance-wallet/manifest.json",
+  "/finance-wallet/icon-192.png",
+  "/finance-wallet/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
